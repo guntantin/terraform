a742a9a157423764e91255974e0745c7b40a73ed
Artak <you@example.com>
2024-03-15 16:34:39 +0400

    fix errors plan

--- project/main.tf
+++ project/main.tf
@@ -45,3 +45,48 @@ module "asg" {
   alb_target_group_arn       = module.alb.alb_target_group_arn
   inctance_security_group_id = module.security_group.inctance_security_group_id
 }
+
+resource "aws_iam_role" "ecr_role" {
+  name = "ECRRole"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ecr.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ECRPolicy"
+  description = "IAM policy for ECR"
+  
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_attachment" {
+  policy_arn = aws_iam_policy.ecr_policy.arn
+  role       = aws_iam_role.ecr_role.name
+}
